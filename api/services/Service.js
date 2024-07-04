094e1361cc8dcab0ec46127deef3140788d8a1f7
arano1212 <arano1212@gmail.com>
2024-07-03 23:05:55 -0600

    feat: added service

---
+++ api/services/Service.js
@@ -0,0 +1,33 @@
+export default class Service {
+  constructor (model) {
+    this.model = model
+  }
+
+  getAll (query) {
+    return this.model.find(query)
+  }
+
+  getById (id) {
+    return this.model.findById(id)
+  }
+
+  updateById (id, data) {
+    return this.model.findByIdAndUpdate(id, data)
+  }
+
+  create (data) {
+    return this.model.create(data)
+  }
+
+  deleteById (id) {
+    return this.model.findByIdAndDelete(id)
+  }
+
+  softDeleteById (id) {
+    return this.model.findByIdAndUpdate(id, { isDeleted: true }, { new: true })
+  }
+
+  restoreById (id) {
+    return this.model.findByIdAndUpdate(id, { isDeleted: false }, { new: true })
+  }
+}
